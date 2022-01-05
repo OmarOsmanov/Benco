ce48019c4eda2911e44597c539e0e34363e6d5dc
OmarOsmanov <tu201906019@code.edu.az>
2022-01-06 00:46:18 +0400

    first commit

---
+++ WebApplication2/Models/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Settings
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Logo { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string WorkTime { get; set; }
+        public string Info { get; set; }
+    }
+}
